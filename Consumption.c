e75be7191554769f3ead8ad85f587674f7820ddd
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-03-28 23:30:26 +0600

    Create Consumption.c

---
+++ Consumption.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{ 
+double a,b;
+//float b;
+int max;
+double u;
+//int y;
+
+scanf("%lf",& a);
+scanf("%lf",& b);
+//int a=(float)a;
+//u=(float)max;
+double y = (a/b);
+ {
+
+printf("%.3lf km/l\n",y);
+}
+return 0;
+
+}
+
